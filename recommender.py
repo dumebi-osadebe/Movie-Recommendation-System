c8e0efa1467be00410d528b2afecb10a73186c90
Dumebi Osadebe <winifred2412@gmail.com>
2023-01-14 13:07:17 -0500

    creating user-product and product-feature matrices

--- recommender.py
+++ recommender.py
@@ -9,11 +9,11 @@ Created on Mon Jan  9 15:11:14 2023
 import pandas as pd
 from tensorflow.keras import models, layers, utils, backend
 import matplotlib.pyplot as plt
+import numpy as np
 
 # import the preprocessing and text libraries from scikitlearn
-from sklearn.preprocessing import scale
+from sklearn.preprocessing import MinMaxScaler
 from sklearn.decomposition import PCA
-from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.cluster import KMeans
 from sklearn.decomposition import PCA
 from sklearn import metrics
@@ -21,16 +21,15 @@ from sklearn import metrics
 # string manipulation libraries
 import re
 import string
-import nltk
-from nltk.corpus import stopwords
 
-movies = pd.read_csv("./ml-latest-small/movies.csv")
-ratings = pd.read_csv("./ml-latest-small/ratings.csv")
+df_movies = pd.read_csv("./ml-latest-small/movies.csv")
+df_users = pd.read_csv("./ml-latest-small/ratings.csv")
 movie_tags = pd.read_csv("./ml-latest-small/tags.csv")
 
 #for the content based system we will be utilizing the movie genres features
 
-#preprocessing the genres for each movie
+#preprocessing the sci-fi genre for each movie
+
 def processing_text(text: str) -> str:
     """Args:
         text (str): the input text you want to clean
@@ -38,13 +37,82 @@ def processing_text(text: str) -> str:
         str: the cleaned text
     """
     
-    text = re.sub("[|]", "", text)
     text = re.sub("-", "", text)
     
     # return text in lower case and stripped of whitespaces
     return text.lower().strip()
 
-movies["genres"] = movies["genres"].apply(lambda x: processing_text(x))
-
-print(movies["genres"])
+df_movies["genres"] = df_movies["genres"].apply(lambda x: processing_text(x))
     
+# users
+df_users.drop(columns = ["timestamp"], inplace= True)
+
+# match movies only with existing user ratings
+df_users = df_users.merge(df_movies, how = "left")
+
+#make columns of the genres using set() to identify the unique genres
+genres = [i.split("|") for i in df_users["genres"].unique()]
+columns = list(set([i for lst in genres for i in lst]))
+columns.remove("(no genres listed)")
+
+#create Product Feature matrix
+for col in columns:
+    df_movies[col] = df_movies["genres"].apply(lambda x: 1 if col in x else 0)
+    
+
+df_movies.drop(columns = ["genres"], inplace = True)
+
+# create User Product matrix
+tmp = df_users.copy()
+df_users = tmp.pivot_table(index = "userId", columns = "movieId", values = "rating")
+
+missing_cols = list(set(df_users.columns) - set(df_movies.index))
+
+for col in missing_cols:
+    df_users[col] = np.nan
+    
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
